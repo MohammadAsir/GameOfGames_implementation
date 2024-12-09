87627e440122ee8a6235fe327492d4a4cd91e33a
HannahChukwu <hannahchukwu03@gmail.com>
2024-12-09 16:20:16 -0500

    I made the changes

--- GuessTheNumber.java
+++ GuessTheNumber.java
@@ -1,63 +1,85 @@
-import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Random;
+import java.util.Scanner;
+
 public class GuessTheNumber {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
 
         System.out.println("Welcome to the Guess the Number Game!");
         boolean play = true;
 
-        while(play){
-            System.out.println("Please specify a range! ");
-            System.out.println("Lower limit: ");
-            int LowerLim = scanner.nextInt();
+        while (play) {
 
-            System.out.println("Upper limit: ");
-            int UpperLim = scanner.nextInt();
+            System.out.println("Please specify a range!");
+            System.out.print("Lower limit: ");
+            int lowerLimit = scanner.nextInt();
 
-            //Calculating the maximum attempts
+            System.out.print("Upper limit: ");
+            int upperLimit = scanner.nextInt();
 
-            int maximumAttempts = (UpperLim - LowerLim +1) / 2;
-            System.out.println("You have " + maximumAttempts +" attempts to guess the number!");
+            if (upperLimit <= lowerLimit) {
+                System.out.println("Invalid range. Upper limit must be greater than the lower limit.");
+                continue;
+            }
 
-            int numberToGuess = random.nextInt(UpperLim - LowerLim +1) + LowerLim;
+            int numberToGuess = random.nextInt(upperLimit - lowerLimit + 1) + lowerLimit;
+            int maxAttempts = (upperLimit - lowerLimit + 1) / 2;
+            System.out.println("You have " + maxAttempts + " attempts to guess the number!");
+
 
             int attempts = 0;
             boolean guessedCorrectly = false;
 
-            while (attempts < maximumAttempts) {
-                System.out.println("Guess a number between " + LowerLim + " and " + UpperLim + ": ");
-                int guess = scanner.nextInt();
+            while (attempts < maxAttempts) {
+                System.out.print("Guess a number between " + lowerLimit + " and " + upperLimit + ": ");
+                int guess;
 
+                try {
+                    guess = scanner.nextInt();
+                } catch (InputMismatchException e) {
+                    System.out.println("Invalid input. Please enter a valid number.");
+                    scanner.next(); // Clear invalid input
+                    continue;
+                }
+
+                // Validate the guess range
+                if (guess < lowerLimit || guess > upperLimit) {
+                    System.out.println("Invalid input. Please enter a number between " + lowerLimit + " and " + upperLimit + ".");
+                    continue;
+                }
+
+                attempts++;
                 if (guess == numberToGuess) {
-                    System.out.println("Correct!! You guessed the number!");
+                    System.out.println("You Won!");
                     guessedCorrectly = true;
                     break;
+                } else if (guess < numberToGuess) {
+                    System.out.println("Incorrect! The number is higher.");
+
                 } else {
-                    System.out.println(" Wrong guess!! Try again!");
+                    System.out.println("Incorrect! The number is lower.");
                 }
-                attempts++;
 
-                System.out.println("You have " + (maximumAttempts - attempts) + " left.");
+                System.out.println("You have " + (maxAttempts - attempts) + " attempts left.");
             }
-            if(!guessedCorrectly){
-                System.out.println("You ran out of attempts! The correct number was " + numberToGuess);
-            }
-            System.out.println("Do you want to play again? (yes/no): ");
-            String answer = scanner.next();
 
-            if (answer.equalsIgnoreCase("yes")){
-                play = true;
+            if (!guessedCorrectly) {
+                System.out.println("Game Over! The correct number was " + numberToGuess);
             }
-            else {
+
+            System.out.print("Do you want to play again? (yes/no): ");
+
+            String replay = scanner.next().toLowerCase();
+
+            if (replay.equals("no")) {
                 play = false;
                 System.out.println("Thanks for playing!");
             }
         }
         scanner.close();
-
-
     }
 }
